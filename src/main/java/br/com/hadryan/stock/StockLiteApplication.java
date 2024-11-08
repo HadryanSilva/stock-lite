02052e5f8c1fb48b771e9e3d37f9cf2f414feb6d
Hadryan Silva <hadryan2001@gmail.com>
2024-11-08 17:28:03 -0300

    Initial commit

---
+++ src/main/java/br/com/hadryan/stock/StockLiteApplication.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hadryan/stock/StockLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
